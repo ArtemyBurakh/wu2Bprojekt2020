b3518946d20308877b0fb59e7ad7bcd98560f6e9
The Haruspex <60507065+ArtemyBurakh@users.noreply.github.com>
2020-02-18 11:41:31 +0100

    v9

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 var style="null";
 var type="null";
 var output="null";
-var pattern =  0;
 var finalName = "null";
 var title = "null";
 var result = false;
@@ -44,15 +43,20 @@ var rusticlf=["iels", "hop", "cot", "ot", "ing", "ins", "in", "ne", "e", "ane", 
 var rusticSuffixes=["son", "son", "sson", "dottir", "datr", "doter", "bern", "barn", "kin",  "", "", "", "", "", "", "", "", "", "","", "", "", "", "", "", "", "", "", "", "","","", "", "", "", "", "", "", "", "", "",""];
 
 //Nordic Syllables
-var nordicff=["Yen", "Tet", "Mak", "Tsi", "Kha", "Lar"];
-var nordicfl=["ta"];
+var nordicff=["Abe", "Jen", "Ad", "Ae", "Ag", "Ag", "Ag", "Al", "Alf", "Alf", "Alv", "And", "And", "An", "Ar", "Arn", "As", "Ast", "Avu", "Ba", "Bal", "Bei", "Ben", "Ber", "Bjor", "Bjö", "Bjä", "Bod", "Bor", "Bre", "Bri", "Bryn", "Bul", "Har", "Dag", "Dan", "Dor", "Dra", "Dre", "Ed", "Ei", "Em", "Er", "Ev", "Fad", "Far", "Fell", "Fre", "Fro", "Fru", "Ful", "Gal", "Gei", "Ger", "Gis", "Gja", "Gol", "Gun", "Gre", "Gros", "Gun", "Gun", "Gun", "Had", "Had", "Hel", "Haj", "Har", "Hei", "Hei", "Hel", "Hen", "Hem", "Hert", "Horn", "Hjö", "Hjä", "Hor", "Her", "Hro", "Hul", "Id", "Ida", "Ig", "Igr", "Ing", "Ing", "Jer", "Jör", "Kar", "Kar", "Kje", "Kja", "Kyr", "Lam", "Lei", "Lem", "Lil", "Lis", "Lis", "Lod", "Lok", "Lond", "Lyd", "Läd", "Låd", "Mar", "Mar", "Mar", "Mat", "Mat", "Mau", "Må", "Mø", "Lø", "Tø", "Nar", "Når", "Nik", "Nil", "Nik", "Nor", "Od", "Og", "Ol", "On", "Or", "Jal", "Jol", "Jöl", "Jor", "Jor", "Kli", "Lam", "Leif", "Lef", "Lei", "Läg", "Lod", "Lor", "Mar", "Mar", "Mav", "Mik", "Mik", "Nar", "Nil", "Pon", "Pan", "Pån", "Rag", "Rev", "Ris", "Räv", "Ror", "Run", "Rus", "Sab", "Sör", "Sib", "Sig", "Sig", "Sig", "Sin", "Søn", "Søl", "Tal", "Tek", "Tem", "Tor", "Tør", "Tat", "Töt", "Tör", "Tho", "Tho", "Tyr", "Tor", "Tor", "Tor", "Tsu", "Uf", "Uf", "Ulf", "Uv", "Yng", "Yng", "Ys", "Wil", "Wil", "Wulf", "Wul", "Vor", "Vid", "Vid", "Vång", "Val", ""];
+var nordicfl=["", "" , "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",  "", "", "ta", "var", "vild", "la", "na", "ni", "nis", "ing", "rinn", "va", "vor", "ders", "ders", "durs", "dörs", "nor", "ke", "fur", "sur", "ar", "nir", "gruf", "nar", "bus", "tild", "tilda", "or", "si", "na", "tur", "or", "lam", "il", "id", "fri", "lund", "lund", "lund", "tar", "ti", "ja", "ond", "wulf", "ling", "ing", "frek", "der", "dis", "gar", "ny", "ur", "en", "fa", "ifa", "mar", "id", "grim", "lund", "di", "ik", "kur", "is", "dis", "ida", "lk", "kas", "rd", "red", "ryd", "dir", "rig", "ta", "ka", "nar", "hem", "heim", "hjem", "hejmm", "ing", "ing", "jmar", "har", "dur", "ak", "ak", "rm", "ach", "gor", "ka", "ta", "ta", "jar", "jar", "rum", "ing", "var", "nar", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "ing", "ga", "ra", "rid", "na", "tar", "tär", "la", "hor", "dis", "den", "ic", "ik", "rik", "leif", "ta", "ta", "rl", "la", "ar", "eld", "eld", "ud", "lak", "ir", "ir", "dar", "yr", "ust", "rs", "ur", "arr", "elf", "kil", "bet", "i", "und", "heim", "en", "ul", "ga", "jar", "ring", "ga", "var", "mir", "ilf", "n"];
 var nordiclf=["Mi", "Dan"];
 var nordicll=["al", "sy", "ma", "dr"];
 
-var southernff=[];
-var southernfl=[];
+var southernff=["Moh", "You", "Ah", "Mah", "Mus", "Yas", "Tah", "Kha", "Ham", "Bil", "Ib", "Hass", "Hus", "Kar", "Tar", "Ab", "Ali", "Sel", "Abd", "Meh", "Om", "Mur", "Ra", "Sa", "Bra", "Dja", "Fat", "Sa", "Fat", "Mer", "Ai", "Kar", "Ke", "Nad", "Sha", "Fat", "Ma", "Re", "Far", "Ay", "Sha", "Ash", "Sah", "Fat", "Dal", "Doh", "Faj", "Su", "Ro", "Hos", "Has", "Gam", "Hab"];
+var southernfl=["del-Rah", "am", "ed", "sef", "sef", "sal-Ya", "am-Kha", "med", "moud", "ta", "fa", "sin", "ha", "led", "za", "al", "him", "san", "sein", "im", "eq", "del", "man", "ar", "ad", "dal", "lah", "chid", "him", "mel", "ima", "iha", "ara", "cha", "ina", "ri", "ma", "i", "maa", "em", "hd", "ra", "qat", "har", "in", "al", "ha", "an", "iny", "naa", "ila", "al", "at", "bib", "a", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
 var southernlf=[];
 var southernll=[];
+
+var easternff=[];
+var easternfl=[];
+var easternlf=[];
+var easternll=[];
 
 //Tiles
 var militaryTitlePrefixes=["Grand ", "Great ", "First ", "Second ", "High ", "Vice-",  "Acting ", "Honorary ",  "", "", "", "", "", "", "", "", "", "", "",  "", "", "", "", "", "", ""];
@@ -63,7 +67,7 @@ var royalTitles=["King", "Queen", "Emperor", "Empress", "Regent", "Prince", "Pri
 var  royalSuffixes=["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "X","XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "", "", "", ""];
 
 var englishPrefixesUni=["Deep", "Reach", "Sky", "Knife", "Bleak", "Blind", "Bone", "Boulder", "Brine", "Brittle", "Broken", "Bronze", "Chill", "Crags", "Crag", "Cron", "Crystal", "Dark", "Dusk", "Elder", "Fallow", "Fore", "Gloom", "Gray", "Green", "Honey", "Moss", "Night", "Pine", "Pure", "Raven", "Reach", "Rime", "Shadow", "Shimmer", "Snap", "Still", "Sunder", "White", "Wolf", "Autumn", "Wind", "Ice", "Bone", "Dragon", "Elders", "North", "Shear", "Sky", "Chill", "Corpe", "Merry", "Drift", "Fell", "Mist", "Bleak", "Steam", "Stone", "Ember", "Gloom", "Golden", "Red", "Banner", "Bleak", "Wind", "Cliff", "Iron", "River", "Arc", "Shriek", "Snow", "Sand", "Leaf", "Bile", "Sun", "Heart", "Moor", "Doom", "Blue", "Double", "Cold", "Black", "Blood", "Bramble", "Break", "Brittle", "Little", "Deep", "Dun", "Field", "Finger", "Fyre", "Fire", "Gray", "Hay", "Kings", "Queens", "Leaf", "Red", "Sand", "Shin", "Side", "Tide", "Timber", "Toad", "Under", "Well", "Wander", "Hack", "Tre", "Tarn", "Pen", "Nant", "Aber"];
-var englishSuffixesUni=["stad", "star", "glow", "gate", "wind", "spire", "point", "coast", "chill", "fall", "shin", "wind", "lane", "lan", "wallow", "drift", "shade", "water", "glow", "gleam", "vangr", "stone", "moril", "sake", "reach", "winter", "wall", "water", "strand", "gale", "moon", "peak", "water", "scar", "cliff", "water", "green", "rock", "mist", "leg", "fringe", "fall", "stone", "skull", "shade", "spring", "helm", "water", "watch", "tooth", "wind", "point", "born", "tand", "furrow", "light", "frost", "shod", "let", "light", "shade", "watch", "call", "sand", "waste", "scorch", "scorn", "wind", "crag", "hill", "arch", "spring", "mist", "flow", "gulch", "water", "shard", "bound", "rock", "point", "wind", "hills", "mist", "bound", "helm", "mere", "grave", "wood", "moon", "claw", "leaf", "crust", "main", "run", "point", "neck", "coal", "fish", "fathom", "scorn", "barrow", "house", "bowl", "fire", "light", "mead", "crest", "stand", "guard", "pall", "tow", "bone", "way", "mark", "spring", "worth", "win", "wick", "weald", "non", "more"];
+var englishSuffixesUni=["stad", "star", "glow", "gate", "wind", "spire", "point", "coast", "chill", "fall", "shin", "wind", "lane", "lan", "wallow", "drift", "shade", "water", "glow", "gleam", "vangr", "stone", "moril", "sake", "reach", "winter", "wall", "water", "strand", "gale", "moon", "peak", "water", "scar", "cliff", "water", "green", "rock", "mist", "leg", "fringe", "fall", "stone", "skull", "shade", "spring", "helm", "water", "watch", "tooth", "wind", "point", "born", "tand", "furrow", "light", "frost", "shod", "let", "light", "shade", "watch", "call", "sand", "waste", "scorch", "scorn", "wind", "crag", "hill", "arch", "spring", "mist", "flow", "gulch", "water", "shard", "bound", "rock", "point", "wind", "hills", "mist", "bound", "helm", "mere", "grave", "wood", "moon", "claw", "leaf", "crust", "main", "run", "point", "neck", "coal", "fish", "fathom", "scorn", "barrow", "house", "bowl", "fire", "light", "mead", "crest", "stand", "guard", "pall", "tow", "bone", "way", "mark", "spring", "worth", "win", "wick", "weald", "non", "more", "", "", "", "", ""];
 var englishPrefixesSep=[];
 var englishSuffixesSep=[];
 
@@ -73,7 +77,7 @@ var rivers=["River", "River", "River", "River", "Creek", "Brook", "Run"];
 var lakes=["Lake", "Lake", "Lake", "Lake", "Pond", "Basin", "Pool"];
 var seas=["Ocean", "Sea"];
 var valleys=["Dale", "Valley", "Vale", "Dell", "Pass", "Crevice"];
-var mountains=["Mountain", "Peak", "Hill", "Ridge"];
+var mountains=["Mount",  "Mount", "Mount", "Mount", "Hill", "Ridge"];
 var caves=["Cave", "Cave", "Cave", "Grotto", "Hollow", "Cavern", "Depths", "Slope", "Burrow", "Caverns", "Depths"];
 var ravines=["Ravine", "Canyon", "Crevice"];
 var peaks=["Peak", "Point", "Bluff"];
@@ -117,6 +121,11 @@ function genPname(){
         midName = nordicff[getRndInteger(0, nordicff.length - 1)] + nordicfl[getRndInteger(0, nordicfl.length - 1)] + nordicfl[getRndInteger(0, nordicfl.length - 1)];
         lastName = nordiclf[getRndInteger(0, nordiclf.length - 1)] + nordicll[getRndInteger(0, nordicll.length - 1)] + nordicll[getRndInteger(0, nordicll.length - 1)];
         familyName = nordiclf[getRndInteger(0, nordiclf.length - 1)] + nordicll[getRndInteger(0, nordicll.length - 1)] + nordicll[getRndInteger(0, nordicll.length - 1)];
+    } else if (style=="southern") {
+        firstName = southernff[getRndInteger(0, southernff.length - 1)] + southernfl[getRndInteger(0, southernfl.length - 1)] + southernfl[getRndInteger(0, southernfl.length - 1)];
+        midName = southernff[getRndInteger(0, southernff.length - 1)] + southernfl[getRndInteger(0, southernfl.length - 1)] + southernfl[getRndInteger(0, southernfl.length - 1)];
+        lastName = southernlf[getRndInteger(0, southernlf.length - 1)] + southernll[getRndInteger(0, southernll.length - 1)] + southernll[getRndInteger(0, southernll.length - 1)];
+        familyName = southernlf[getRndInteger(0, southernlf.length - 1)] + southernll[getRndInteger(0, southernll.length - 1)] + southernll[getRndInteger(0, southernll.length - 1)];  
     };
 
     //Arranage the names based on type 
@@ -153,9 +162,6 @@ function genPname(){
     
     //Checks the output against a number regex that prevent unpronouncable names from being generated, unless the style is set to otherworldly
      
-    if (style=="otherworldly") {
-      
-    } else {
          //Tests for three hard consonants in a row
          result = regex3C.test(output);
 
@@ -175,7 +181,7 @@ function genPname(){
        } else {
 
        };
-    };
+
 
     //Checks the output agains two regex that prevent grammatically unsound names from being generated, such as names ending in - or '
 
@@ -214,89 +220,7 @@ function genPname(){
 };
 
 function genNatureName() {
-    var style=document.getElementById("NatureNameStyle").value;
-    var type=document.getElementById("NatureNameType").value;
-    
-    pattern=getRndInteger(0,8);
-
-     if (style=="elegant") {
-        
-        if (getRndInteger(0,2)==1) {
-            natureName = elegantff[getRndInteger(0, elegantff.length - 1)] + elegantfl[getRndInteger(0, elegantfl.length - 1)] + elegantfl[getRndInteger(0, elegantfl.length - 1)];
-        } else {
-            natureName = elegantlf[getRndInteger(0, elegantlf.length - 1)] + elegantll[getRndInteger(0, elegantll.length - 1)] + elegantll[getRndInteger(0, elegantll.length - 1)];
-        };
-        
-    } else if (style=="evil") {
-        
-        if (getRndInteger(0,2)==1) {
-            natureName = evilff[getRndInteger(0, evilff.length - 1)] + evilfl[getRndInteger(0, evilfl.length - 1)] + evilfl[getRndInteger(0, evilfl.length - 1)];
-        } else {
-            natureName = evillf[getRndInteger(0, evillf.length - 1)] + evilll[getRndInteger(0, evilll.length - 1)] + evilll[getRndInteger(0, evilll.length - 1)];
-        };
-
-    } else if (style=="rustic") {
-        
-        if (getRndInteger(0,1)==1) {
-            natureName = rusticff[getRndInteger(0, rusticff.length - 1)] + rusticfl[getRndInteger(0, rusticfl.length - 1)] + rusticfl[getRndInteger(0, rusticfl.length - 1)];
-        } else {
-            natureName = rusticll[getRndInteger(0, rusticll.length - 1)] + rusticlf[getRndInteger(0, rusticlf.length - 1)] + rusticlf[getRndInteger(0, rusticlf.length - 1)];
-        };
-        
-    } else if (style=="nordic") {
-        
-        if (getRndInteger(0,2)==1) {
-            natureName = nordicff[getRndInteger(0, nordicff.length - 1)] + nordicfl[getRndInteger(0, nordicfl.length - 1)] + nordicfl[getRndInteger(0, nordicfl.length - 1)];
-        } else {
-            natureName = nordiclf[getRndInteger(0, nordiclf.length - 1)] + nordicll[getRndInteger(0, nordicll.length - 1)] + nordicll[getRndInteger(0, nordicll.length - 1)];
-        };
-
-    } else if (style=="english") {
-        natureName = englishPrefixesUni[getRndInteger(0, englishPrefixesUni.length - 1)] + englishSuffixesUni[getRndInteger(0, englishSuffixesUni.length - 1)];
-    } ; 
-
-    if (type=="river") {
-        natureWord=rivers[getRndInteger(0, rivers.length - 1)];
-    } else if (type=="lake") {
-        natureWord=lakes[getRndInteger(0, lakes.length - 1)];
-    } else if (type=="sea") {
-        natureWord=seas[getRndInteger(0, seas.length - 1)];
-    } else if (type=="valley") {
-
-    } else if (type=="mountain") {
-
-    } else if (type=="cave") {
-        natureWord=caves[getRndInteger(0, caves.length - 1)];
-    } else if (type=="ravine") {
-
-    } else if (type=="peak") {
-
-    } else if (type=="clearing") {
-
-    } else if (type=="desert") {
-
-    } else if (type=="forest") {
-
-    };
-
-    if (style=="english") {
-        if (pattern<2) {
-            finalName = natureWord + " " + natureName; 
-        } else {
-            finalName =  natureName + " " + natureWord;
-        };
-    } else {
-        if (pattern<3) {
-            finalName = natureWord + " " + natureName;
-        } else if (pattern>3 && pattern<6) {
-            finalName =  natureName + " " + natureWord;
-        } else if (pattern==7) {
-            finalName = natureName + "'s " + natureWord;
-        } else if (pattern==8) {
-            finalName = natureWord + " of " + natureName;
-        };    
-    };
-
-    //Display output
-    document.getElementById('nameout').innerHTML =  finalName;
+    var style = document.getElementById("NatureNameStyle").value;
+    var type = document.getElementById("NatureNameType").value;
+    var pattern = getRndInteger(0,9); 
 };
